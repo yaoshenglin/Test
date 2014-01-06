922d7d832610fb634fc4f39132aad95e3ad00484
Yinhaibo <Yinhaibo@YinhaibotekiMacBook-Pro.local>
2014-01-06 16:04:20 +0200

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by Yinhaibo on 14-1-6.
+//  Copyright (c) 2014年 Yinhaibo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
