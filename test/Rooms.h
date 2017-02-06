772bf8e16a376cb86f04a890496a3aaa8655982c
yaoshenglin <yaoshenglin@qq.com>
2017-02-06 15:23:12 +0800

    0

---
+++ test/Rooms.h
@@ -0,0 +1,46 @@
+//
+//  Rooms.h
+//  test
+//
+//  Created by Yin on 14-7-6.
+//  Copyright (c) 2014年 Yinhaibo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EnumTypes.h"
+
+@interface Rooms : NSObject
+{
+    @public
+}
+
+@property (nonatomic) int ID;
+@property (nonatomic) int age;
+@property (retain, nonatomic) NSString *name;
+@property (retain, nonatomic) NSString *date;
+
+- (id)initWithID:(int)theID name:(NSString *)theName;
+- (void)StartRun;
+- (void)setNumber:(CGFloat)value;
+- (void)printValue:(CGFloat)value;
+- (NSDate *)dateFromString:(NSString *)str withDateFormater:(NSString *)formater;
+- (NSString *)getDeviceVersionInfo;
+- (NSString *)correspondVersion;
++ (NSString *)dateToString:(NSDate *)date withDateFormater:(NSString *)formater;
++ (NSData *)ValidCRCWithHost:(NSData *)data;
+
+
+#pragma mark request
++ (void)startRequest;
++ (NSArray *)requestWithUrl:(NSString *)urlString;
++ (NSArray *)ParseXmlWithString:(NSString *)stringL;
+
++ (NSArray *)ParseDataFromIFace;
+
++ (void)PostJson;
+
++ (NSDate *)dateWith:(NSString *)hourStr week:(Week)weekDay;
+
++ (void)addProperty:(NSString *)propertyName;
+
+@end
